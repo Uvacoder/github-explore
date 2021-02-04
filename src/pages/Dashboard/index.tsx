b5685d244e7f38024afcbc0998941c720a4602a1
jaimelay <jaimelay1997@gmail.com>
2021-02-03 21:29:16 -0300

    feat: add structure of dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,70 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/16366726?s=460&u=a470a73a5629f08282f479d5f8b319d432b692be&v=4"
+            alt="Jaime Lay"
+          />
+          <div>
+            <strong>jaimelay/competitive-programming</strong>
+            <p>
+              Algorithms used in contests and solutions of some problems that I
+              solved in competitive programming.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/16366726?s=460&u=a470a73a5629f08282f479d5f8b319d432b692be&v=4"
+            alt="Jaime Lay"
+          />
+          <div>
+            <strong>jaimelay/competitive-programming</strong>
+            <p>
+              Algorithms used in contests and solutions of some problems that I
+              solved in competitive programming.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/16366726?s=460&u=a470a73a5629f08282f479d5f8b319d432b692be&v=4"
+            alt="Jaime Lay"
+          />
+          <div>
+            <strong>jaimelay/competitive-programming</strong>
+            <p>
+              Algorithms used in contests and solutions of some problems that I
+              solved in competitive programming.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
